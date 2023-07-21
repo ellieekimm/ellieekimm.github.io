2743a061d61bc1c5cb8de589fc0ab882067e9988
Ellie Kim <elliekim100@gmail.com>
2023-07-21 14:27:54 -0400

    Finalization

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,6 @@
 var rightArr = [];
-
+var rightString = "";
+var charWrong = 0;
 function typingTest(){
     $("#baconButton").text("You got this!");
     $('#baconButton').css("background-color", "pink");
@@ -13,11 +14,21 @@ function typingTest(){
         var character = event.key;
         userInput.push(character);
         if (currentIndex === (rightArr.length - 1)) {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 },
+              });
             const end = performance.now();
             const difference = end - startTimes[0];
-            var speed = (currentIndex / (difference / 1000)).toFixed(0);
-            var endingMessage = "Characters per Second: " + speed;
-            $("#baconIpsumOutput").text(endingMessage);
+            var words = (currentIndex / 5).toFixed(0);
+            var seconds = difference / 1000;
+            var speed = (words / seconds) * 60;
+            console.log(charWrong/currentIndex);
+            console.log(charWrong);
+            var accuracy = (1 - (charWrong / currentIndex)) * 100;
+            var endingMessage = "<span class = 'finalmessage'>" + "Words per Minute: " + speed.toFixed(0) + "<br>Accuracy: " + accuracy.toFixed(0) + "%"+ "</span>";
+            $("#baconIpsumOutput").html(endingMessage);
             $("html").addClass("congrats");
             setTimeout(function() {
                 $("html").removeClass("congrats");
@@ -30,14 +41,17 @@ function typingTest(){
                 setTimeout(function() {
                     $("html").removeClass("game-over");
                 }, 1000);
-                var audio = new Audio("sounds/wrong.mp3");
+                var audio = new Audio("../sounds/wrong.mp3");
                 audio.play();
-                $("#baconIpsumOutput").text("Try Again with no mistakes next time!");
-                $("#baconButton").text("Start!");
-                $('#baconButton').css("background-color", "darkgreen");
-                userInput = [];
+                charWrong++;
+                $("#word-count").text("Character Count: " + (currentIndex + 1));
+                $("#baconIpsumOutput").html(rightString.slice(0, currentIndex) + '<span id="incorrect">' + rightString[currentIndex] + '</span>' +
+                rightString.slice(currentIndex + 1));
+                currentIndex++;
             } else {
                 $("#word-count").text("Character Count: " + (currentIndex + 1));
+                $("#baconIpsumOutput").html(rightString.slice(0, currentIndex) + '<span class="correct">' + rightString[currentIndex] + '</span>' +
+                rightString.slice(currentIndex + 1));
                 currentIndex++;
             }
     }});
@@ -46,19 +60,15 @@ function typingTest(){
 $("#baconButton").click(function() {
     $.getJSON('https://baconipsum.com/api/?callback=?', { 'type':'meat-and-filler', 'start-with-lorem':'1', 'sentences':'1' }, function(baconGoodness) {
         if (baconGoodness && baconGoodness.length > 0) {
-            $("#baconIpsumOutput").html('');
-            for (var i = 0; i < baconGoodness.length; i++) {
-                $("#baconIpsumOutput").append(baconGoodness[i]);
-            }
-            $("#baconIpsumOutput").show();
-
-            var rightString = baconGoodness[0];
+            $("#baconIpsumOutput").html(baconGoodness[0]);
+            rightString = baconGoodness[0];
             rightArr = rightString.split("");
         }
     });
     userInput = [];
     typingTest();
 });
+
 
 
 
